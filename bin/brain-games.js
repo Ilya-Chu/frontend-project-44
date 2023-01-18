2423d2b8f8b1a7e9ec9445806db9717e51412418
il_ch <zyhoneec@gmail.com>
2023-01-18 23:46:10 +0300

    added greeting to brain-games, added readline-sync package

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!')  
+
+import { userName } from "../src/cli.js";
+
+console.log('Welcome to the Brain Games!');
+console.log('Hi ' + userName + '!');
