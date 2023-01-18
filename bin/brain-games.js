7ea008fd9722ad072b2b6d1f737281165e3e2481
il_ch <zyhoneec@gmail.com>
2023-01-19 00:48:41 +0300

    added eslint and CodeClimate Maintability

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { userName } from "../src/cli.js";
+import userName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-console.log('Hi ' + userName + '!');
+console.log(`Hi ${userName}!`);
--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,5 @@
 import readlineSync from 'readline-sync';
 
-export const userName = readlineSync.question('May I have your name? ');
+const userName = readlineSync.question('May I have your name? ');
+
+export default userName;
