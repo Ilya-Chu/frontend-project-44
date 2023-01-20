aa1ba00bd65b11ef151d70553c7e97cb77500f63
il_ch <zyhoneec@gmail.com>
2023-01-20 05:30:18 +0300

    fix extra space in Question brain-progression

--- src/games/progression-g.js
+++ src/games/progression-g.js
@@ -5,8 +5,8 @@ const rules = 'What number is missing in the progression?';
 const nFinder = (start, step, n) => start + (step * (n - 1));
 
 const getProgression = (start, step, length, hiddenNum) => {
-  let result = '';
-  for (let i = 1; i <= length; i += 1) {
+  let result = String(start);
+  for (let i = 2; i <= length; i += 1) {
     if (i === hiddenNum) {
       result += ' ..';
     } else {
@@ -20,7 +20,7 @@ const questionAnswer = () => {
   const starNum = randomInteger(1, 10);
   const randStep = randomInteger(1, 5);
   const randLength = randomInteger(5, 10);
-  const randHiddenNum = randomInteger(1, randLength);
+  const randHiddenNum = randomInteger(2, randLength);
   const question = getProgression(starNum, randStep, randLength, randHiddenNum);
   const answer = nFinder(starNum, randStep, randHiddenNum);
   return gameData(question, String(answer));
