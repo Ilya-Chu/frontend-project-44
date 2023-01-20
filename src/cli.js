2458f7a6e6620db4e69085676f48089548995512
il_ch <zyhoneec@gmail.com>
2023-01-20 05:08:10 +0300

    fixed name brain-GCD to brain-gcd

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const startBrainGame = () => {
+export const startBrainGame = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
