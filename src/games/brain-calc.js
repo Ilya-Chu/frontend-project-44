c7f8b8e8a0e12235197bae36fffbba87a609b137
il_ch <zyhoneec@gmail.com>
2023-05-17 15:16:40 +0600

    refactored brain-calc

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -7,13 +7,10 @@ const doArithmetic = (num1, num2, operator = '+') => {
   switch (operator) {
     case '+':
       return num1 + num2;
-      break;
     case '-':
       return num1 - num2;
-      break;
     case '*':
       return num1 * num2;
-      break;
     default:
       throw new Error(`Unexpected operator: ${operator}`);
   }
